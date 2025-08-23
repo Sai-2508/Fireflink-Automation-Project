4426184989a0c4cbcaaf895e6770148b67438a88
Penugonda Sai Durga <Penugonda Sai Durga@FFE-Penugonda-Sai-Durga>
2025-08-23 11:58:51 +0530

    JUst for junkins execution

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -2,6 +2,16 @@ import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
-  public void f() {
+  public void Hello() 
+  {
+
+	  System.out.println("Hello");
+	  
+	  System.out.println("Welcome");
+	  
+	  
+	  System.out.println("just for junkins");
+	  
+  
   }
 }
